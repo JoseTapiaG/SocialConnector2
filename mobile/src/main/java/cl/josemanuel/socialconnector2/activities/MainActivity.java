0a55b9037868b95a17d92390792e4a5e82a9f8f0
JoseTapiaG <jose.wt@gmail.com>
2017-04-23 05:04:21 -0300

    Commit inicial

---
+++ mobile/src/main/java/cl/josemanuel/socialconnector2/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package cl.josemanuel.socialconnector2.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cl.josemanuel.socialconnector2.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
