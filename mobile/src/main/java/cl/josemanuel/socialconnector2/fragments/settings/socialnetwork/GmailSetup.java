10bb50a6be1b48c7fbbd5221eff95bb35a35a20d
Felipe Rodriguez <farodrig92@gmail.com>
2018-03-19 11:49:31 -0300

    Created initial gmail behaviour

--- mobile/src/main/java/cl/josemanuel/socialconnector2/fragments/settings/socialnetwork/GmailSetup.java
+++ mobile/src/main/java/cl/josemanuel/socialconnector2/fragments/settings/socialnetwork/GmailSetup.java
@@ -3,6 +3,8 @@ package cl.josemanuel.socialconnector2.fragments.settings.socialnetwork;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -22,35 +24,14 @@ public class GmailSetup extends SocialNetworkSetup {
         setListener((View view, String pass) -> {
             GmailSetup setup = this;
             try {
-                String result = SocialNetworkService.configure(this.getId(), pass);
+                String url = SocialNetworkService.configure(this.getId(), pass);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("http://www.google.cl"));
+                view.getContext().startActivity(i);
             } catch (Exception e) {
-                LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-                final View dialogView = inflater.inflate(R.layout.settings_code_dialog, null);
-                // Inflate and set the layout for the dialog
-                // Pass null as the parent view because its going in the dialog layout
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                builder.setView(dialogView);
-
-                builder
-                        .setTitle("Contraseña no reconocida")
-                        .setMessage("Ingrese nuevamente la contraseña.")
-                        .setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                final EditText code = (EditText) dialogView.findViewById(R.id.settings_code);
-                                Toast toast = Toast.makeText(view.getContext(), code.getText(), Toast.LENGTH_SHORT);
-                                toast.show();
-                                setup.getListener().apply(view, code.getText().toString());
-                            }
-                        })
-                        .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                String msg = "Error conectando al servidor. Intente de nuevo más tarde.";
+                Toast toast = Toast.makeText(view.getContext(), msg, Toast.LENGTH_LONG);
+                toast.show();
             }
             return null;
         });
